dca3330723641d1c43c3ad6bffbda6d3fac96dee
Aastha240 <minhasaastha6@gmail.com>
2024-01-26 23:43:18 -0500

    ICE1

--- ICE1-Starter/ICE1-Starter/list.js
+++ ICE1-Starter/ICE1-Starter/list.js
@@ -4,48 +4,48 @@
 //Description: we are adding 3 new items to the list in the below code.
 
 
-// Get the reference to the HTML list with the id "theList".
+// get the reference to the HTML list with the id "myList".
 let list = document.getElementById("myList");
 
-// Adding "Mango" to the end of the list.
-// Create a new list item element.
+// adding "Mango" to the end of the list.
+// create a new list item element.
 let element = document.createElement("li"); 
 
-// Create a text node with the content "Mango".
+// create a text node with the content "Mango".
 let text = document.createTextNode("Mango"); 
 
- // Append the text node to the list item.
+ // append the text node to the list item.
 element.appendChild(text);
 
-// Append the list item to the end of the list.
+// append the list item to the end of the list.
 list.appendChild(element); 
 
-// Adding "Pineapple" to the end of the list.
-// Create a new list item element.
+// adding "Pineapple" to the end of the list.
+// create a new list item element.
 element = document.createElement("li"); 
 
-// Create a text node with the content "Pineapple".
+// create a text node with the content "Pineapple".
 text = document.createTextNode("Pineapple"); 
 
-// Append the text node to the list item.
+// append the text node to the list item.
 element.appendChild(text); 
 
-// Append the list item to the end of the list.
+// append the list item to the end of the list.
 list.appendChild(element); 
 
 
-// Adding "Blueberry" to the start of the list.
- // Create a new list item element.
+// adding "Blueberry" to the start of the list.
+ // create a new list item element.
 element = document.createElement("li");
 
- // Create a text node with the content "Blueberry".
+ // create a text node with the content "Blueberry".
 text = document.createTextNode("Blueberry");
 
- // Append the text node to the list item.
+ // append the text node to the list item.
 element.appendChild(text);
 
-// Obtain the list's first <li> element.
+// obtain the list's first <li> element.
 let firstListItem = list.firstChild;
 
-// Place the new item in the list before the first item.
+// place the new item in the list before the first item.
 list.insertBefore(element, firstListItem);
